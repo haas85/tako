b5d6bb986f6a89df090d9968d6766b5bad0cc390
haas85 <ingonza85@gmail.com>
2014-04-10 16:08:23 +0200

    Version 1.1.2

--- package/js/tako.debug.js
+++ package/js/tako.debug.js
@@ -1,4 +1,4 @@
-/* TaKo v1.1.1 - 03/04/2014
+/* TaKo v1.1.2 - 10/04/2014
    http://takojs.com
    Copyright (c) 2014 Iñigo Gonzalez Vazquez <ingonza85@gmail.com> (@haas85) - Under MIT License */
 (function() {
