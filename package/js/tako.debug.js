a845cc242b985663f2bf958cc67c9e8d4e6bd4d8
haas85 <ingonza85@gmail.com>
2014-11-20 23:05:15 +0100

    Improved android notifications

--- package/js/tako.debug.js
+++ package/js/tako.debug.js
@@ -218,8 +218,7 @@
         cb = callbacks[_i];
         cb.call(cb);
       }
-      ready = true;
-      return $("body").attr("data-os", "android");
+      return ready = true;
     };
     _navigate = function(action, target, query) {
       var nav;
