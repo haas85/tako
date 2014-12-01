d1198ebb8db3fd485ad466b61f3fece21a5341ee
haas85 <ingonza85@gmail.com>
2014-12-01 20:32:39 +0100

    WP forms, lists

--- package/js/tako.debug.js
+++ package/js/tako.debug.js
@@ -1,4 +1,4 @@
-/* TaKo v1.2.1 - 30/11/2014
+/* TaKo v1.2.1 - 01/12/2014
    http://takojs.com
    Copyright (c) 2014 Iñigo Gonzalez Vazquez <ingonza85@gmail.com> (@haas85) - Under MIT License */
 (function() {
@@ -215,7 +215,8 @@
         cb = callbacks[_i];
         cb.call(cb);
       }
-      return ready = true;
+      ready = true;
+      return $("body").attr("data-os", "wp");
     };
     _navigate = function(action, target, query) {
       var nav;
